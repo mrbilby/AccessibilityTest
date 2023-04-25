3260c025156120826d4883c77a8106517c3ccf62
mrbilby <baileymrb@gmail.com>
2023-04-25 08:26:01 +0100

    Initial Commit

---
+++ AccessibilityTest/AccessibilityTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityTestApp.swift
+//  AccessibilityTest
+//
+//  Created by James Bailey on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
